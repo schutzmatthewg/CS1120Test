c72fc0dfe4f78e3093c6c817a1c88045ac4be01e
schut <schut@10.80.149.216>
2017-03-01 11:15:08 -0500

    Created a new constructor and expression object

--- CS1120Test/src/edu/wmich/cs1120/test/Expression.java
+++ CS1120Test/src/edu/wmich/cs1120/test/Expression.java
@@ -1,5 +1,15 @@
 package edu.wmich.cs1120.test;
 
 public class Expression {
-
+	
+	private int op1;
+	private int op2;
+	private char operand;
+	public Expression(int op1, int op2, char operand){
+		this.op1 = op1;
+		this.op2 = op2;
+		this.operand = operand;
+	}
+	
+	public Expression(){}
 }
